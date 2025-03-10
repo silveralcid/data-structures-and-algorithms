ae60c80066fc58632ff29c93730a9ef7f3fe76c3
Silver Alcid <silveralcid@gmail.com>
2025-03-10 11:30:51 -0400

    Update twosum-brute.js

--- twosum-brute.js
+++ twosum-brute.js
@@ -1,6 +1,6 @@
 var twoSum = function(nums, target) {
-  for (i = 0; i < nums.length; i++) {
-    for (j = i + 1; j < nums.length; j++) {
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
       if (nums[i] + nums[j] === target) {
         return [i , j]
       }
